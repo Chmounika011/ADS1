6e41478a5612e464f75c6f47a443e66c01480fe2
Chmounika011 <chmounika0011@gmail.com>
2018-10-06 10:44:03 -0700

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ module4/4.2 Evaluate Postfix/Solution.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+//package week1;
+
+import java.util.Stack;
+
+ class Postfixeval
+{
+	public Double evalution(String exp)
+	{
+		Stack<Double> s=new Stack<Double>();
+		char []c=exp.toCharArray();
+		int n=c.length;
+		for(int i=0;i<n;i++)
+		{
+			char ch=c[i];
+			if(isOperator(ch))
+			{
+				switch(ch)
+				{
+				  case '+': 
+					  s.push(s.pop() + s.pop());
+				  			break;
+                  case '*':
+                	  s.push(s.pop() * s.pop());     
+                  			break;
+                  case '-':
+                	  s.push(-s.pop() + s.pop());   
+                  			break;
+                  case '/':
+                	  s.push(1 / s.pop() * s.pop());
+                  			break;
+				}
+			} 
+			else if(Character.isDigit(ch)) 
+			{
+               
+                s.push(0.0);
+                while (Character.isDigit(c[i]))
+                    s.push(10.0 * s.pop() + (c[i++] - '0'));
+            }
+        }
+		if (!s.isEmpty()) 
+            return s.pop();
+        else
+            return 0.0;
+    }
+
+
+		
+	private boolean isOperator(char ch) 
+	{
+		// TODO Auto-generated method stub
+		 return ch == '*' || ch == '/' || ch == '+' || ch == '-';
+	}
+}
+
+public class Solution {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String exp;
+		exp=sc.nextLine();
+		Postfixeval s=new Postfixeval();
+		System.out.println(s.evalution(exp));
+		
+		
+	}
+
+}
