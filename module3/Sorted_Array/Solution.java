9fa5a08ef5bd7df221c9a3c361ef992d4558fdb1
Chmounika011 <chmounika0011@gmail.com>
2018-10-06 00:31:02 -0700

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- module3/Sorted_Array/Solution.java
+++ module3/Sorted_Array/Solution.java
@@ -2,17 +2,17 @@
 
 import java.util.Scanner;
 
-public class Solution2 {
+public class Solution {
 
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
-		System.out.println("enter number of elements in array1");
+		//System.out.println("enter number of elements in array1");
 		int n=sc.nextInt();
-		System.out.println("enter number of elements in array2");
+		//System.out.println("enter number of elements in array2");
 		int m=sc.nextInt();
-		System.out.println("enter elements");
+		//System.out.println("enter elements");
 		int [] a=new int[n];
 		int []b=new int[m];
 		int []c=new int[n+m];
